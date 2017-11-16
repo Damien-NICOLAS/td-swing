b13870439232c8e9b83910d5a44e7e104b985926
damien <dnicolas@etud.insa-toulouse.fr>
2017-11-16 23:54:38 +0100

    Avant modification pour espacer les labels

--- src/main/java/com/fredericboisguerin/insa/calculateurprix/CalculateurPrixPresenter.java
+++ src/main/java/com/fredericboisguerin/insa/calculateurprix/CalculateurPrixPresenter.java
@@ -7,15 +7,70 @@ public class CalculateurPrixPresenter {
         this.calculateurPrixView = calculateurPrixView;
     }
 
-    public void onComputeButtonClicked(String montantArticleAsText, String quantiteArticleAsText) {
+    public void onComputeButtonClicked(String montantArticleAsText, String quantiteArticleAsText, String nomDuPays) {
         float montantArticleAsFloat = Float.parseFloat(montantArticleAsText);
         int quantiteArticleAsInt = Integer.parseInt(quantiteArticleAsText);
 
+        Pays pays = Pays.valueOf(nomDuPays);
+
+        double tauxDeTaxe = 0;
+
+
+        switch (pays){
+            case Allemagne:
+                tauxDeTaxe = 1.19;
+                break;
+            case Belgique:
+                tauxDeTaxe = 1.21;
+                break;
+            case Danemark:
+                tauxDeTaxe = 1.25;
+                break;
+            case Grèce:
+                tauxDeTaxe = 1.23;
+                break;
+            case Espagne:
+                tauxDeTaxe = 1.21;
+                break;
+            case France:
+                tauxDeTaxe = 1.20;
+                break;
+            case Irlande:
+                tauxDeTaxe = 1.23;
+                break;
+            case Italie:
+                tauxDeTaxe = 1.22;
+                break;
+            case Chypre:
+                tauxDeTaxe = 1.19;
+                break;
+            case Luxenbourg:
+                tauxDeTaxe = 1.15;
+                break;
+            case Pays_Bas:
+                tauxDeTaxe = 1.21;
+                break;
+            case Autriche:
+                tauxDeTaxe = 1.20;
+                break;
+            case Portugal:
+                tauxDeTaxe = 1.23;
+                break;
+            case Suède:
+                tauxDeTaxe = 1.25;
+                break;
+
+                default:
+                    tauxDeTaxe = 1;
+
+        }
+
         float montantHTAsFloat = montantArticleAsFloat * quantiteArticleAsInt;
+        float montantTTCAsFloat = (float) (montantHTAsFloat * tauxDeTaxe);
 
 
         calculateurPrixView.afficherMontantHorsTaxe(montantHTAsFloat);
-
+        calculateurPrixView.afficherMontantTouteTaxeComprise(montantTTCAsFloat);
 
 
 
--- src/main/java/com/fredericboisguerin/insa/calculateurprix/CalculateurPrixView.java
+++ src/main/java/com/fredericboisguerin/insa/calculateurprix/CalculateurPrixView.java
@@ -24,14 +24,21 @@ public class CalculateurPrixView extends JFrame {
     private JLabel montantHTLabel = new JLabel("Montant HT : ");
     private JFormattedTextField montantHTTextField = new JFormattedTextField(NumberFormat.getCurrencyInstance());
 
+    private JLabel montantTTCLabel = new JLabel("Montant TTC : ");
+    private JFormattedTextField montantTTCTextField = new JFormattedTextField(NumberFormat.getCurrencyInstance());
+
+    private JLabel paysLabel = new JLabel("Pays");
+    private JComboBox<String> paysCombo;
+
     private JButton computeButton = new JButton("Calculer");
 
     private JPanel contentPane = new JPanel();
 
-    private JPanel labelPane = new JPanel(new GridLayout(0, 1));
+    private JPanel labelPane = new JPanel();
+    private GridLayout labelPaneGridLayout = new GridLayout(0, 1);
 
-    private JPanel fieldPane = new JPanel(new GridLayout(0, 1));
-
+    private JPanel fieldPane = new JPanel();
+    private GridLayout fieldPaneGridLayout = new GridLayout(0, 1);
 
 
 
@@ -46,23 +53,50 @@ public class CalculateurPrixView extends JFrame {
         nombreArticleLabel.setLabelFor(nombreArticleTextField);
         nombreArticleTextField.setToolTipText("Entrez ici la quantité d'article voulue ");
 
-        montantHTTextField.setValue(15);
+
+        String[] pays = {"Belgique","Danemark","Allemagne","Grèce","Espagne","France","Irlande","Italie","Chypre","Luxenbourg","Pays_Bas","Autriche","Portugal","Suède"};
+        paysLabel.setLabelFor(paysCombo);
+        paysCombo = new JComboBox<>(pays);
+        paysCombo.setToolTipText("Sélectionnez ici le pays de votre achat");
+        paysCombo.setSelectedIndex(5);
+
+
+        montantHTTextField.setValue(0);
         montantHTTextField.setEditable(false);
         montantHTLabel.setLabelFor(montantHTTextField);
 
+        montantTTCTextField.setValue(0);
+        montantTTCTextField.setEditable(false);
+        montantTTCLabel.setLabelFor(montantTTCTextField);
 
-        computeButton.addActionListener(e -> this.presenter.onComputeButtonClicked(prixArticleTextField.getText(), nombreArticleTextField.getText()));
+
+
+
+
+        computeButton.addActionListener(e -> this.presenter.onComputeButtonClicked(prixArticleTextField.getText(), nombreArticleTextField.getText(), paysCombo.getItemAt(paysCombo.getSelectedIndex())));
 
         setContentPane(contentPane);
+
+        labelPaneGridLayout.setHgap(5);
+        fieldPaneGridLayout.setHgap(5);
+
+        labelPane.setLayout(labelPaneGridLayout);
+        fieldPane.setLayout(fieldPaneGridLayout);
 
 
         labelPane.add(prixArticleLabel);
         labelPane.add(nombreArticleLabel);
+        labelPane.add(paysLabel);
         labelPane.add(montantHTLabel);
+        labelPane.add(montantTTCLabel);
+
+
 
         fieldPane.add(prixArticleTextField);
         fieldPane.add(nombreArticleTextField);
+        fieldPane.add(paysCombo);
         fieldPane.add(montantHTTextField);
+        fieldPane.add(montantTTCTextField);
 
         contentPane.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         add(labelPane, WEST);
@@ -80,15 +114,13 @@ public class CalculateurPrixView extends JFrame {
 
     public void afficherMontantHorsTaxe(float leMontantHTAsFloat){
 
-
-
         montantHTTextField.setValue(leMontantHTAsFloat);
 
+    }
 
+    public void afficherMontantTouteTaxeComprise(float leMontantTTCAsFloat){
 
-
-
-
+        montantTTCTextField.setValue(leMontantTTCAsFloat);
 
     }
 
